70c1ef5e0055b244224812aeee8cc5e6f6bd0da1
stevencol <steven2088@hotmail.es>
2023-09-07 16:02:45 -0500

    Carga de proyecto

---
+++ src/main/java/com/libros/libros/configuration/UploadConfi.java
@@ -0,0 +1,15 @@
+package com.libros.libros.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class UploadConfi     {
+
+
+
+
+
+}
